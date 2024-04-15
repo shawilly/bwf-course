3465eb9bdf98fe8737b763b1ce321c049a163fb2
Shane Williams <shanebarrywilliams@gmail.com>
2024-04-15 21:55:53 +0100

    NextUI to page
    Using instead of material ui as it supports ts and nextjs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,15 @@
+import { NextUIProvider } from "@nextui-org/react";
 import { Suspense } from "react";
 import { Hero } from "../components/Hero";
 
 export default function Home() {
   return (
+      <NextUIProvider> 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Suspense
-        fallback={<div className="text-4xl text-white">loading...</div>}
-      >
+        <Suspense fallback={<div className="text-4xl text-white">loading...</div>}>
         <Hero />
       </Suspense>
-    </main>
+   </main>
+   </NextUIProvider>
   );
 }
